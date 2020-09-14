dcb0c32346686cc62d2debc2026cf956f3c9e955
binyi <binyi@atguigu.com>
2020-09-14 11:27:17 +0800

    hotfix第三次提交

--- cloud-provider-payment8001/src/test/java/MyTest.java
+++ cloud-provider-payment8001/src/test/java/MyTest.java
@@ -2,11 +2,10 @@ import org.junit.Test;
 
 /**
  * @author BinYi
- * @create 2020-09-14 09:50
  */
 public class MyTest {
     @Test
     public void t1(){
-        System.out.println("hfjkfhkjf");
+        System.out.println("hfjkfhkjf");//hotfix测试
     }
 }
